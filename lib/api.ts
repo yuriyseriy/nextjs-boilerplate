df9f01e92fc43b1f5bcbb4f7673a110d86eb61f1
Yuriy Seriy <yuriyseriy1993@gmail.com>
2023-11-28 16:20:01 +0300

    next public url and env

--- lib/api.ts
+++ lib/api.ts
@@ -1,7 +1,6 @@
-const NEXT_PUBLIC_URL = process.env.NEXT_PUBLIC_URL;
-
 const _postRequest = async (url: string, params: any) => {
-  const res = await fetch(`${NEXT_PUBLIC_URL}/api/${url}`, {
+  console.log(process.env)
+  const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/${url}`, {
     method: "POST",
     headers: {
       "Accept": "application/json",
